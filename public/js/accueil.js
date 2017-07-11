eb603ba430ab4664d86b001ee85795e0040278a5
guillaumeboucaud <guillaumeboucaud@outlook.com>
2017-07-11 17:04:11 -0400

    Test implementation AJAX

---
+++ public/js/accueil.js
@@ -0,0 +1,141 @@
+// Menu Facture
+$('#facture').click(function(){  
+	var data = {};
+	$.ajax({
+		url: 'facture',
+		data: data,
+		method: 'POST'
+	}).then(function (response) {
+		if(response.statut == 'succes') {
+			$("#body-page").empty();
+			$("#body-page").append("<p style=\"text-align: center; color: white;\">" + response.contenu + "</p>")
+		}
+		else {
+			alert('Erreur');
+		}
+	}).catch(function (err) {
+		console.error(err);
+	});
+});  
+
+// Menu Horaire
+$('#horaire').click(function(){  
+	var data = {};
+	$.ajax({
+		url: 'horaire',
+		data: data,
+		method: 'POST'
+	}).then(function (response) {
+		if(response.statut == 'succes') {
+			$("#body-page").empty();
+			for(var i = 0; i < 5; i++){
+				$("#body-page").append($("<p style=\"text-align: center; color: white;\">").text(response.contenu + ' ' + i));
+			}
+		}
+		else {
+			alert('Erreur');
+		}
+	}).catch(function (err) {
+		console.error(err);
+	});
+});       
+   
+// Menu inscription
+$('#inscription').click(function(){  
+	var data = {};
+	$.ajax({
+		url: 'inscription',
+		data: data,
+		method: 'POST'
+	}).then(function (response) {
+		if(response.statut == 'succes') {
+			$("#body-page").empty();
+			for(var i = 0; i < 2; i++){
+				$("#body-page").append($("<p style=\"text-align: center; color: white;\">").text(response.contenu + ' ' + i));
+			}
+		}
+		else {
+			alert('Erreur');
+		}
+	}).catch(function (err) {
+		console.error(err);
+	});
+});     
+   
+// Menu désinscription
+$('#desinscription').click(function(){  
+	var data = {};
+	$.ajax({
+		url: 'desinscription',
+		data: data,
+		method: 'POST'
+	}).then(function (response) {
+		if(response.statut == 'succes') {
+			$("#body-page").empty();
+			for(var i = 0; i < 4; i++){
+				$("#body-page").append($("<p style=\"text-align: center; color: white;\">").text(response.contenu + ' ' + i));
+			}
+		}
+		else {
+			alert('Erreur');
+		}
+	}).catch(function (err) {
+		console.error(err);
+	});
+});   
+   
+// Menu relevé de notes
+$('#relevedenotes').click(function(){  
+	var data = {};
+	$.ajax({
+		url: 'relevedenotes',
+		data: data,
+		method: 'POST'
+	}).then(function (response) {
+		if(response.statut == 'succes') {
+			$("#body-page").empty();
+			
+			$("#body-page").append($("<p style=\"text-align: center; color: white;\">").text(' '));
+			
+			/*
+			//Exemple de génération de tableau du côté client
+			var table = $('<table></table>').addClass('notes').attr('id','notes');
+			for (var i = 0; i < 10; i++) 
+			{
+                row = $('<tr></tr>');
+                for (var j = 0; j < 3; j++) 
+				{
+                    var rowData = $('<td></td>').addClass('bar').text(response.contenu + ' ' + i);
+                    row.append(rowData);
+                }
+                table.append(row);
+            }
+			$("#body-page").append(table);
+			*/
+			$("#body-page").append(response.contenu);
+		}
+		else {
+			alert('Erreur');
+		}
+	}).catch(function (err) {
+		console.error(err);
+	});
+});   
+   
+// Barre de menu
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+$('ul li a').click(function() {
+    $(this).parent().find('ul.sub-menu').toggle();
+    return false;
+});
+
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
--- server.js
+++ server.js
@@ -1,101 +1,171 @@
-var express = require('express'),
-    fs = require('fs'),
-    rl = require('readline'),
-    interface = require("./lib/steeven_bot/steven_bot.js"),
-    processeur = require("./lib/processeur/processeur.js"),
-    gw2requete = require("./lib/gw2SpidyRequete.js"),
-    logger = require('./lib/logger.js'),
-    logpath = './server.log',
-    server = express();
-
-server.set('port', (process.env.PORT || 5000));
-server.use(express.static(__dirname + '/public'));
-
-function logToJson(logfile, callback){
-    var ret = [];
-    var linereader = rl.createInterface({
-        input: fs.createReadStream('./server.log')
-    });
-
-    linereader.on('line', function(line){
-        ret.push(JSON.parse(line));
-    });
-
-    linereader.on('close', function(){
-        callback(JSON.stringify(ret, null, 2));
-    });
-}
+/////////////////////////////////////
+// Import des librairies nécessaires
+/////////////////////////////////////
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 
 
-server.get('/', function(req, res, cb){
-    logger.log('info', "Requete HTTP recu par GET \'/\'.")
-	res.writeHeader(200, {"Content-type":"text/html"});
-	res.write("Le serveur est en vie!");
-	res.end();
+/////////////////////////////////////
+// Représente l'application
+/////////////////////////////////////
+var portail = express();
+
+
+/////////////////////////////////////
+// Settings
+/////////////////////////////////////
+portail.engine('.html', require('ejs').__express); // Pas sûr de cette ligne
+portail.set('views', __dirname + '/views');
+portail.set('view engine', 'html');
+portail.use(express.static(path.join(__dirname, 'public')));
+portail.use(bodyParser.urlencoded({extended: false}));
+portail.use(bodyParser.json());
+
+
+/////////////////////////////////////
+// Page de bienvenue 
+/////////////////////////////////////
+portail.get('/', function(req, res) 
+{	
+    console.log('Bienvenue sur l\'application Portail Etudiant');
 	
-	return cb();
+    // Affiche la page connexion.html
+	res.render('connexion');
 });
 
-server.get('/log', function(req, res, cb){
-    logToJson(logpath, function(result){
-        res.writeHeader(200, {"Content-type":"application/json"});
-        res.write(result);
-        res.end();
 
-        return cb();
-    });
+/////////////////////////////////////
+// Gestion des GET
+/////////////////////////////////////
+portail.get('/connexion', function(req, res) 
+{	
+    console.log('Page de connexion');
+
+    // Affiche la page connexion.html
+    res.render('connexion');
 });
 
-function reponsehttp(res, body) {
-    res.writeHeader(200, {"Content-type":"application/json"});
-    res.write(body);
-    res.end();
-};
 
-server.get('/gw2requete/tous', function(req, res, cb){
-    reponsehttp(res, JSON.stringify(gw2requete.tousLesObjets(), null, 2));
-    return cb();
+portail.get('/accueil', function(req, res)
+{
+	// Affiche la page accueil.html
+	res.render('accueil');
 });
 
-server.get('/gw2requete/type', function(req, res, cb){
-    reponsehttp(res, JSON.stringify(gw2requete.obtenirMapType(), null, 2));
-    return cb();
+
+portail.get('/facture', function(req, res)
+{
+	// Affiche la page facture.html
+	res.render('facture');
 });
 
-server.get('/gw2requete/rarete', function(req, res, cb){
-    reponsehttp(res, JSON.stringify(gw2requete.obtenirMapRarete(), null, 2));
-    return cb();
+
+portail.get('/horaire', function(req, res)
+{
+	// Affiche la page horaire.html
+	res.render('horaire');
 });
 
-server.get('/gw2requete/type/:type', function(req, res, cb){
-    reponsehttp(res, JSON.stringify(gw2requete.parType(req.params.type), null, 2));
-    return cb();
+
+//////////////////////////////////////////////////////////
+// Gestion des POST
+//////////////////////////////////////////////////////////
+
+// Gestion POST page connexion
+portail.post('/connexion', function(req, res) {
+    
+    var code = req.body.code;
+    var nip = req.body.nip;
+    
+    console.log("Code permanent: %s", code);
+    console.log("NIP:            %s", nip);
+    
+	// TODO: déplacer la validation dans un autre module	
+    // Si champs valides, retourne le statut (succes) et l'url de redirection
+	if(code == 'admin' && nip == 'admin')
+	{
+		res.json({statut: "succes", redirection: 'accueil'});
+	}
+    // Sinon, retourne le statut (erreur) et un message d'erreur à afficher 
+    else
+	{
+		res.json({statut: "erreur", message: 'Informations erron&eacute;es!'});
+	}
 });
 
-server.get('/gw2requete/rarete/:rarete', function(req, res, cb){
-    reponsehttp(res, JSON.stringify(gw2requete.parRarete(req.params.rarete), null, 2));
-    return cb();
+
+// Gestion POST facture page d'accueil
+portail.post('/facture', function(req, res) {
+
+	res.json({statut: "succes", contenu: 'Facture'});
 });
 
-server.get('/gw2requete/objet/:nom', function(req, res, cb){
-    reponsehttp(res, JSON.stringify(gw2requete.parNom(req.params.nom), null, 2));
-    return cb();
+
+// Gestion POST horaire page d'accueil
+portail.post('/horaire', function(req, res) {
+
+	res.json({statut: "succes", contenu: 'Horaire'});
 });
 
-server.get('/test_proc', function(req, res, cb){
-    res.writeHeader(200, {"Content-type":"application/json"});
-    res.write('LISTE CHANGEMENT PRIX #########################\n' +
-              JSON.stringify(processeur.changementPrix(50), null, 2) + 
-              '\nMEILLEUR PROFITS ##################\n' +
-              JSON.stringify(processeur.meilleursProfits(50), null, 2)
-    );
-    res.end();
 
-    return cb();
+// Gestion POST inscription page d'accueil
+portail.post('/inscription', function(req, res) {
+
+	res.json({statut: "succes", contenu: 'Inscription'});
 });
 
-server.listen(server.get('port'), function(){
-	logger.log('info', "le server ecoute sur: %s", server.get('port'));
+
+// Gestion POST désinscription page d'accueil
+portail.post('/desinscription', function(req, res) {
+
+	res.json({statut: "succes", contenu: 'Désinscription'});
 });
 
-gw2requete.initialiser();
+
+// Gestion POST relevé de notes page d'accueil
+portail.post('/relevedenotes', function(req, res) 
+{	
+	// TODO : Interroger la bd
+	// Exemple de données récupérées de la bd
+	var items = [
+    {code: 'INF1120', name: 'Programmation I', description: 'A+'}, 
+    {code: 'INF1130', name: 'Mathématiques pour informaticien', description: 'A'}, 
+    {code: 'INF4375', name: 'Paradigmes des échanges Internet', description: 'A-'}, 
+    {code: 'INM5151', name: 'Projet d\'analyse et de modélisation', description: 'B+'}, 
+    {code: 'INF2120', name: 'Programmation II', description: 'B'}, 
+	];
+
+	// TODO : Vérifier l'existence de données
+	//        Si aucune donnée, retourner un statut erreur et un message à afficher
+	//		  res.json({statut: "erreur", contenu: 'Vous n'avez aucune note de disponible'});
+	// Construction du tableau html correspondant
+	
+	// TODO : extraire la fonction dans un autre module
+	// TODO : gérer les caractères accentués
+	var resultat = '';
+	resultat += '<table class=\"notes\">';
+	resultat += '<col width="75"><col width="200"><col width="75">';
+	resultat += '<tr><th>Sigle</th><th>Nom</th><th>Note</th></tr>';
+	
+	var i;
+	for(i in items)
+	{
+		resultat += '<tr>';
+		resultat += '<td>' + items[i].code + '</td>';
+		resultat += '<td>' + items[i].name + '</td>';
+		resultat += '<td>' + items[i].description + '</td>';
+		resultat += '</tr>';
+	}
+	resultat += '</table>';
+	 
+	// Retourne le tableau html généré avec les données de la bd
+	res.set({ 'content-type': 'application/json; charset=utf-8' });
+	res.json({statut: "succes", contenu: resultat});
+});
+
+
+//////////////////////////////////////////////////////////
+// Démarrage de l'application
+//////////////////////////////////////////////////////////
+portail.listen(8080);
