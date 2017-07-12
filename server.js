802eb6f41858b7027795e38f0e8d65fecbc66477
Julien Labrosse <neiluj@gmail.com>
2017-07-11 22:22:58 -0400

    shit's working son

--- server.js
+++ server.js
@@ -2,14 +2,32 @@ var express = require('express');
 var server = express();
 
 server.set('port', 8080);
-server.use(express.static(__dirname + '/views/html'));
 
-server.get('/', function(req, res, cb){
-    res.sendFile('form.html');
-    
-    return cb();
+//probably useless example function
+function reponsehttp(res, body) {
+    res.writeHeader(200, {"Content-type":"text/html"});
+    res.write(body);
+    res.end;
+};
+
+//example 1
+server.get('/', function(req, res){
+    console.log("hi");
+    res.sendFile(__dirname + '/testFile.html');
 });
 
+//example 2
+server.get('/oi', function(req, res){
+    reponsehttp(res, "Hello World");
+});
+
+//real shit son
+server.get('/home', function(req, res){
+    res.sendFile(__dirname + '/views/html/form.html');
+});
+
+
+//do not touch
 server.listen(server.get('port'), function(){
-    console.log("le server ecoute sur: %s", server.get('port'));
+    console.log("server is listening on: %s", server.get('port'));
 });
